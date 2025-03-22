6a9da4d265f53aa9026b2a9ccd38a127b8949bcd
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-03-22 15:02:08 +0100

    Création des icônes SVG pour les contacts

---
+++ src/services/PDFService/icons/ContactIcons.jsx
@@ -0,0 +1,136 @@
+// src/services/PDFService/icons/ContactIcons.jsx
+
+import React from 'react';
+import { Svg, Path, G, Circle } from '@react-pdf/renderer';
+
+/**
+ * Composant d'icône générique pour les contacts
+ * @param {Object} props - Propriétés du composant
+ * @param {string} props.color - Couleur de l'icône
+ * @param {number} props.size - Taille de l'icône
+ * @param {string} props.backgroundColor - Couleur de fond (optionnelle)
+ * @param {React.ReactNode} props.children - Le contenu SVG de l'icône
+ * @returns {React.ReactElement} - Composant SVG
+ */
+export const ContactIcon = ({ color = 'white', size = 16, backgroundColor, children }) => {
+  const withCircle = backgroundColor !== undefined;
+  
+  return (
+    <Svg width={size} height={size} viewBox="0 0 24 24">
+      {withCircle && (
+        <Circle cx="12" cy="12" r="10" fill={backgroundColor} />
+      )}
+      <G fill={color}>{children}</G>
+    </Svg>
+  );
+};
+
+/**
+ * Icône Email
+ */
+export const EmailIcon = (props) => (
+  <ContactIcon {...props}>
+    <Path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+  </ContactIcon>
+);
+
+/**
+ * Icône Téléphone
+ */
+export const PhoneIcon = (props) => (
+  <ContactIcon {...props}>
+    <Path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
+  </ContactIcon>
+);
+
+/**
+ * Icône Site web
+ */
+export const WebsiteIcon = (props) => (
+  <ContactIcon {...props}>
+    <Path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm6.93 6h-2.95c-.32-1.25-.78-2.45-1.38-3.56 1.84.63 3.37 1.91 4.33 3.56zM12 4.04c.83 1.2 1.48 2.53 1.91 3.96h-3.82c.43-1.43 1.08-2.76 1.91-3.96zM4.26 14C4.1 13.36 4 12.69 4 12s.1-1.36.26-2h3.38c-.08.66-.14 1.32-.14 2 0 .68.06 1.34.14 2H4.26zm.82 2h2.95c.32 1.25.78 2.45 1.38 3.56-1.84-.63-3.37-1.9-4.33-3.56zm2.95-8H5.08c.96-1.66 2.49-2.93 4.33-3.56C8.81 5.55 8.35 6.75 8.03 8zM12 19.96c-.83-1.2-1.48-2.53-1.91-3.96h3.82c-.43 1.43-1.08 2.76-1.91 3.96zM14.34 14H9.66c-.09-.66-.16-1.32-.16-2 0-.68.07-1.35.16-2h4.68c.09.65.16 1.32.16 2 0 .68-.07 1.34-.16 2zm.25 5.56c.6-1.11 1.06-2.31 1.38-3.56h2.95c-.96 1.65-2.49 2.93-4.33 3.56zM16.36 14c.08-.66.14-1.32.14-2 0-.68-.06-1.34-.14-2h3.38c.16.64.26 1.31.26 2s-.1 1.36-.26 2h-3.38z" />
+  </ContactIcon>
+);
+
+/**
+ * Icône LinkedIn
+ */
+export const LinkedInIcon = (props) => (
+  <ContactIcon {...props}>
+    <Path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z" />
+  </ContactIcon>
+);
+
+/**
+ * Icône GitHub
+ */
+export const GitHubIcon = (props) => (
+  <ContactIcon {...props}>
+    <Path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.342-3.369-1.342-.454-1.155-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.268 2.75 1.026A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.026 2.747-1.026.546 1.377.202 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.161 22 16.416 22 12c0-5.523-4.477-10-10-10z" />
+  </ContactIcon>
+);
+
+/**
+ * Icône Localisation
+ */
+export const LocationIcon = (props) => (
+  <ContactIcon {...props}>
+    <Path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 0 1 0-5 2.5 2.5 0 0 1 0 5z" />
+  </ContactIcon>
+);
+
+/**
+ * Icône Twitter/X
+ */
+export const TwitterIcon = (props) => (
+  <ContactIcon {...props}>
+    <Path d="M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z" />
+  </ContactIcon>
+);
+
+/**
+ * Icône Document/CV
+ */
+export const DocumentIcon = (props) => (
+  <ContactIcon {...props}>
+    <Path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z" />
+  </ContactIcon>
+);
+
+// Mapping des types d'icônes aux composants
+export const contactIconComponents = {
+  email: EmailIcon,
+  phone: PhoneIcon,
+  website: WebsiteIcon,
+  linkedin: LinkedInIcon,
+  github: GitHubIcon,
+  location: LocationIcon,
+  twitter: TwitterIcon,
+  document: DocumentIcon
+};
+
+/**
+ * Sélectionne l'icône appropriée selon le type
+ * @param {string} type - Type d'icône
+ * @param {Object} props - Propriétés à passer à l'icône
+ * @returns {React.ReactElement|null} - Composant d'icône ou null si type non reconnu
+ */
+export const getContactIcon = (type, props = {}) => {
+  const IconComponent = contactIconComponents[type.toLowerCase()];
+  if (!IconComponent) return null;
+  
+  return <IconComponent {...props} />;
+};
+
+export default {
+  ContactIcon,
+  EmailIcon,
+  PhoneIcon,
+  WebsiteIcon,
+  LinkedInIcon,
+  GitHubIcon,
+  LocationIcon,
+  TwitterIcon,
+  DocumentIcon,
+  getContactIcon
+};
